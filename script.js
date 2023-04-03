6a52abd7491aeae2f6a1791f894cb55fd0bd64fb
a3if <atif.arif702@gmail.com>
2023-04-04 03:28:36 +0500

    update mobile-nav background

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 window.onload = function () {
-  const menu_btn = document.querySelector(".hamburger");
-  const mobile_menu=document.querySelector('.mobile-nav')
-  menu_btn.addEventListener("click", function () {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-  });
-
-  mobile_menu.addEventListener("click", function () {
-    menu_btn.classList.toggle("is-active");
-    mobile_menu.classList.toggle("is-active");
-  });
-};
-
+    const menu_btn = document.querySelector(".hamburger");
+    const mobile_menu = document.querySelector(".mobile-nav");
+  
+    // Event listener on hamburger button
+    menu_btn.addEventListener("click", function () {
+      menu_btn.classList.toggle("is-active");
+      mobile_menu.classList.toggle("is-active");
+  
+      // Check if hamburger button is active
+      if (menu_btn.classList.contains("is-active")) {
+        document.body.classList.add("no-scroll");
+      } else {
+        document.body.classList.remove("no-scroll");
+      }
+    });
+  
+    mobile_menu.addEventListener("click", function () {
+      menu_btn.classList.toggle("is-active");
+      mobile_menu.classList.toggle("is-active");
+      document.body.classList.remove("no-scroll");
+    });
+  };
